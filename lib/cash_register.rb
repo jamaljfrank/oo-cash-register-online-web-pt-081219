2f55c307e7559b34c8581cc86f989e96ee45de45
Jamal Frank <jamaljfrank@gmail.com>
2019-09-29 11:27:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
     def add_item(item, price, quantity = 1)
         self.total += (price * quantity)
         self.last_transaction = price * quantity
-        self.items.fill(item, self.items.length, quantity)
+        self.items.fill(item, self.items.count, quantity)
     end
 
     def apply_discount
